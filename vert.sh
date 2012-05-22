b2e87eeaaa426d9aac9c6eb6d4afb1e14ef22a66
Anders Conbere <aconbere@gmail.com>
2012-05-22 09:35:56 -0500

    YAY

--- vert.sh
+++ vert.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-LUA_VERSION="5.1.5"
-LUA_URL="http://www.lua.org/ftp/lua-$LUA_VERSION.tar.gz"
-
 CURRENT_DIR=`pwd`
-LUAROCKS_URI='https://github.com/keplerproject/luarocks/tarball/master'
 
 if [[ ! -e './build' ]]
 then
@@ -16,32 +12,100 @@ then
   mkdir './.vert'
 fi
 
-if [[ ! -e "./build/lua-$LUA_VERSION.tar.gz" ]]
-then
-  wget $LUA_URL -O "./build/lua-$LUA_VERSION.tar.gz"
-fi
+build_lua () {
+  # $1 = lua version to install ex: 5.1.5, 5.2
+  # $2 = platform to install on ex: linux, solaris, bsd
+  # $3 = directory to install to ex: ./
+
+  if [[ ! "$1" ]]
+  then
+    echo "build_lua takes 3 arguments given: 0"
+  fi
+  local version=$1
+  
+  if [[ ! "$2" ]]
+  then
+    echo "build_lua takes 3 arguments given: 1"
+  fi
+  local platform=$2
+
+  if [[ ! "$3" ]]
+  then
+    echo "build_lua takes 3 arguments given: 2"
+  fi
+  local target_dir=$3
+
+  local lua_url="http://www.lua.org/ftp/lua-$version.tar.gz"
+
+  if [[ ! -e "./build/lua-$version.tar.gz" ]]
+  then
+    wget $lua_url -O "./build/lua-$version.tar.gz"
+  fi
+
+  tar -xvpf "./build/lua-$version.tar.gz" -C ./build
+
+  mv "./build/lua-$version" './build/lua'
+  pushd './build/lua'
+  make $platform
+  make install INSTALL_TOP="$target_dir"
+  popd
+}
 
 
-tar -xvpf "./build/lua-$LUA_VERSION.tar.gz" -C ./build
+build_luarocks () {
+  # $1 = version: luarocks version to install ex: 5.1.5, 5.2
+  # $2 = prefix: prefix to install luarocks into
+  # $3 = sysconfdir: where to store luarocks config
+  # $4 = with_lua: What lua to use
 
-OUT=`ls ./build | grep lua`
-mv "./build/$OUT" './build/lua'
-pushd './build/lua'
-INSTALL_TOP="$CURRENT_DIR/.vert"
-make local
-popd
+  if [[ ! "$1" ]]
+  then
+    echo "build_luarocks takes 4 arguments given: 0"
+  fi
+  local version=$1
 
-if [[ ! -e './build/luarocks.tar.gz' ]]
-then
-  wget $LUAROCKS_URI -O './build/luarocks.tar.gz'
-fi
+  luarocks_uri='https://github.com/keplerproject/luarocks/tarball/master'
 
-tar -xvpf './build/luarocks.tar.gz' -C './build'
+  if [[ $version == "HEAD" ]]
+  then
+    luarocks_uri='https://github.com/keplerproject/luarocks/tarball/master'
+  fi
+  
+  if [[ ! "$2" ]]
+  then
+    echo "build_luarocks takes 4 arguments given: 1"
+  fi
+  local prefix=$2
 
-OUT=`ls ./build | grep kepler`
-mv "./build/$OUT" './build/luarocks'
+  if [[ ! "$3" ]]
+  then
+    echo "build_luarocks takes 4 arguments given: 2"
+  fi
+  local sysconfdir=$3
 
-pushd './build/luarocks'
-./configure --prefix="$CURRENT_DIR/.vert" --sysconfdir="$CURRENT_DIR/.luarocks" --force-config --with-lua=/usr/local
-make && make install
-popd
+  if [[ ! "$4" ]]
+  then
+    echo "build_luarocks takes 4 arguments given: 3"
+  fi
+  local with_lua=$4
+
+
+  if [[ ! -e './build/luarocks.tar.gz' ]]
+  then
+    wget $luarocks_uri -O './build/luarocks.tar.gz'
+  fi
+
+  tar -xvpf './build/luarocks.tar.gz' -C './build'
+
+  OUT=`ls ./build | grep kepler`
+  mv "./build/$OUT" './build/luarocks'
+
+  pushd './build/luarocks'
+  ./configure --prefix=$prefix --sysconfdir=$sysconfdir --force-config --with-lua=$with_lua
+  make && make install
+  popd
+}
+
+
+build_lua 5.1.5 linux "$CURRENT_DIR/.vert"
+build_luarocks "HEAD" "$CURRENT_DIR/.vert" "$CURRENT_DIR/.luarocks" "$CURRENT_DIR/.vert"
