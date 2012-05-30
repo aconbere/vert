aa704084850b139215a1e57595b7e4e047e5e962
Anders Conbere <aconbere@gmail.com>
2012-05-30 09:26:31 -0500

    Fixing bugs in activate
    * Add a few more paths to the generated path
    * Bump the default lua version to 5.1.5
    * Support absolute path names
    * Update readme

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-= VERT: A virtual environment for lua.
+# VERT: A virtual environment for lua.
 
 Produces a localized install of lua and luarocks for isolated installation and
 running of lua programs.
 
-== Command [vert]
+## Command [vert]
 
 To run vert just run `vert` with the path to the directory to install into
 
@@ -20,3 +20,19 @@ flags `--lua-version` and `--luarocks-version`
     (vert) $> lua
     Lua 5.2.0  Copyright (C) 1994-2011 Lua.org, PUC-Rio
 
+## Activating an environment
+
+There isn't really any magic in Vert, and getting running in a virtual
+environment using is a manual though simple process. After you build a virtual
+env using vert, a shell script will be created in ./bin/activate of that vert.
+To activate the vert execute:
+
+    $> source ./bin/activate
+
+All this script does is copy the current environment variables that setup your
+paths (`PATH`, `LUA_CPATH`, `LUA_PATH` and `PS1`) and reassigning them to point
+to the particular virtual env you're working on.
+
+Once an environment is activate simple run `deactivate` to exit
+
+    (vert)$> deactivate
---
+++ src/utils.lua
@@ -0,0 +1,83 @@
+local http  = require("socket.http")
+local ltn12 = require("ltn12")
+local lfs   = require("lfs")
+local io    = require("io")
+
+local M = {}
+
+function M.ensure(f, err)
+  if not f then
+    print(err)
+    os.exit()
+  end
+end
+
+function M.run(command, ...)
+  local to_run = string.format(command, ...)
+
+  print("EXECUTING:", to_run)
+
+  return os.execute(to_run)
+end
+
+function M.expanddir(directory)
+  if directory then
+    if directory:sub(1,1) == "~" then
+      return os.getenv("HOME")..directory:sub(2, #directory)
+    elseif directory == "." then
+      return lfs.currentdir()
+    elseif directory == ".." then
+      return lfs.currentdir().."../"
+    elseif directory:sub(1,2) == "./" then
+      return lfs.currentdir()..directory:sub(3, #directory)
+    elseif directory:sub(1,1) == "/" then
+      return directory
+    else
+      return lfs.currentdir().."/"..directory
+    end
+  end
+end
+
+function M.download(url, filename)
+  return http.request({ url = url
+                      , method = "GET"
+                      , sink = ltn12.sink.file(io.open(filename, "w"))
+                      })
+end
+
+function M.build_lua(lua_dir, platform, top)
+  local current_dir = lfs.currentdir()
+  lfs.chdir(lua_dir)
+
+  M.ensure(M.run("make %s", platform), "make lua failed")
+  M.ensure(M.run("make install INSTALL_TOP=%s", top),
+         "install lua failed")
+  lfs.chdir(current_dir)
+end
+
+function M.build_luarocks(luarocks_dir, prefix)
+  local current_dir = lfs.currentdir()
+  lfs.chdir(luarocks_dir)
+  M.ensure(M.run("./configure --prefix=%s --sysconfdir=%s --force-config --with-lua=%s",
+             prefix, prefix, prefix),
+             "configure luarocks failed")
+  M.ensure(M.run("make"),
+         "make luarocks failed")
+  M.ensure(M.run("make install"),
+         "install luarocks failed")
+  lfs.chdir(current_dir)
+end
+
+function M.write_activate_script(template, lua_version, prefix)
+  local activate_file, err = io.open(prefix.."/bin/activate", "w+")
+
+  if not activate_file then
+    print("Failed to open activate file: "..err)
+    os.exit(3)
+  end
+
+  activate_file:write(string.format(template, lua_version, prefix))
+  activate_file:close()
+end
+
+return M
